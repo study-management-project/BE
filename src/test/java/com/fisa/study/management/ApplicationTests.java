0af09321df3ccdb92d4e560e818a8b091529e4d4
j0303p <j0303p@gmail.com>
2024-08-18 18:06:00 +0900

    feat: 백업용0818

--- src/test/java/com/fisa/study/management/ApplicationTests.java
+++ src/test/java/com/fisa/study/management/ApplicationTests.java
@@ -1,13 +1,38 @@
 package com.fisa.study.management;
 
+import com.fisa.study.management.domain.room.dto.RoomRequestDTO;
+import com.fisa.study.management.domain.room.service.RoomService;
+import com.fisa.study.management.domain.snapshot.dto.RegSnapshotDTO;
+import com.fisa.study.management.domain.snapshot.service.SnapshotService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class ApplicationTests {
 
+	@Autowired
+	private SnapshotService snapshotService;
+	@Autowired
+	private RoomService roomService;
+
 	@Test
 	void contextLoads() {
+		RoomRequestDTO roomRequestDTO= RoomRequestDTO.builder()
+				.name("test용")
+				.description("테스트테스트")
+				.build();
+		roomService.createRoom(roomRequestDTO);
+//		snapshotService.regSnapshot(1L,
+//				RegSnapshotDTO.builder()
+//						.content("test2")
+//						.build());
+//		snapshotService.regSnapshot(1L,
+//				RegSnapshotDTO.builder()
+//						.content("test3")
+//						.build());
+
 	}
 
 }
