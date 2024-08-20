7d6e1c98646163769dc212ae427f75e52b3a0667
j0303p <j0303p@gmail.com>
2024-08-20 10:22:30 +0900

    feat: checkup comment 테스트추가

--- src/test/java/com/fisa/study/management/InsertDummyData.java
+++ src/test/java/com/fisa/study/management/InsertDummyData.java
@@ -1,5 +1,10 @@
 package com.fisa.study.management;
 
+import com.fisa.study.management.domain.checkup.dto.ReceiveCheckUpDTO;
+import com.fisa.study.management.domain.checkup.entity.CheckUp;
+import com.fisa.study.management.domain.checkup.service.CheckUpService;
+import com.fisa.study.management.domain.comment.dto.CommentDTO;
+import com.fisa.study.management.domain.comment.service.CommentService;
 import com.fisa.study.management.domain.member.dto.MemberRegisterDTO;
 import com.fisa.study.management.domain.member.entity.Member;
 import com.fisa.study.management.domain.member.entity.Role;
@@ -7,10 +12,12 @@ import com.fisa.study.management.domain.member.repository.MemberRepository;
 import com.fisa.study.management.domain.member.service.MemberService;
 import com.fisa.study.management.domain.room.dto.RoomRequestDTO;
 import com.fisa.study.management.domain.room.dto.RoomResponseByAdminDTO;
+import com.fisa.study.management.domain.room.entity.Room;
 import com.fisa.study.management.domain.room.repository.RoomRepository;
 import com.fisa.study.management.domain.room.service.RoomService;
 import com.fisa.study.management.domain.snapshot.dto.RegSnapshotDTO;
 import com.fisa.study.management.domain.snapshot.service.SnapshotService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -21,6 +28,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.Objects;
 import java.util.UUID;
 
 @SpringBootTest
@@ -43,6 +51,12 @@ public class InsertDummyData {
     private UUID roomId = null;
     @Autowired
     private SnapshotService snapshotService;
+
+    @Autowired
+    private CheckUpService checkUpService;
+
+    @Autowired
+    private CommentService commentService;
 
     @Test
     @Order(1)
@@ -83,6 +97,30 @@ public class InsertDummyData {
 
         snapshotService.regSnapshot(userId, dto);
         log.info("Order 3");
+    }
+    @Test
+    @Order(4)
+    public void insertCheckup() throws Exception {
+        Long userId = memberRepository.findByEmail("test").get().getId();
+
+        UUID uuid = roomRepository.findByAdminId(userId).get(0).getUuid();
+
+        ReceiveCheckUpDTO dto = ReceiveCheckUpDTO.builder()
+                .title("test용 checkup")
+                .build();
+        checkUpService.registerCheckUpForRoom(userId, uuid, dto);
+        log.info("Order 4");
+    }
+    @Test
+    @Order(5)
+    public void insertComment(){
+        Long userId = memberRepository.findByEmail("test").get().getId();
+        UUID uuid = roomRepository.findByAdminId(userId).get(0).getUuid();
+        CommentDTO dto= CommentDTO.builder()
+                .uuid(uuid)
+                .content("테스트용 코멘트")
+                .build();
+        commentService.regCommentByRoomId(dto);
     }
 
 }
